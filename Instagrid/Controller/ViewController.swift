59605e36fe51776f372690afa6c81f103386472e
Melvin Poutrel <melvin.poutrel@gmail.com>
2022-06-11 11:46:49 +0200

    Add views

---
+++ Instagrid/Controller/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Melvin Mac on 16/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton?
+    
+    @IBOutlet weak var button2: UIButton?
+    
+    @IBOutlet weak var button3: UIButton?
+    
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func button1Pressed(_ sender: UIButton) {
+        sender.isSelected = true
+        print("Button 1 pressé")
+    }
+    
+    @IBAction func button2Pressed(_ sender: UIButton) {
+        sender.isSelected = true
+        print("Button 2 pressé")
+    }
+    
+    @IBAction func button3Pressed(_ sender: UIButton) {
+        sender.isSelected = true
+        print("Button 3 pressé")
+    }
+}
---
+++ Instagrid/Supporting Files/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Instagrid
+//
+//  Created by Melvin Mac on 16/05/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "mainController")
+        
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
